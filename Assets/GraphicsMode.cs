612a9731878dd04682df4f6217940adb9d45d416
Suma K <sumazmailbox@rediffmail.com>
2020-02-14 20:23:18 +0530

    AR in Botany (initial version)
    AR in Botany - major project (initial version)

---
+++ Assets/GraphicsMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GraphicsMode : MonoBehaviour
+{
+    public void BackCommand()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/HomeMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeMenu : MonoBehaviour
+{
+    public void SelectGraphicsMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SelectVideoMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitCommand()
+    {
+        Debug.Log("Exit Application!");
+        Application.Quit();
+    }
+}
